1d405423cf1c1b8ec98c31ffbe73f9b69b58711d
Raunak Kumar <212152221+codebyraunak@users.noreply.github.com>
2026-05-01 13:36:36 +0530

    Update vite.config.js

--- election-ai-assistant/vite.config.js
+++ election-ai-assistant/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/election-ai-assistant/',
   plugins: [react()],
   server: { port: 3000 },
 })
